f9a35fba591e72b1c1b8aff39cf5f09ae73929bc
MnyZhao <mengyang.zhao@futuremove.cn>
2019-03-11 15:20:03 +0800

    上个版本增加了网络请求超时 去掉debug选中标签

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     print('日志');
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter News',
       theme: ThemeData(
         primarySwatch: Colors.red,
